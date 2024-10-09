6adc5387b34011283aa52d000067a152f8f15bda
HP <HP@HP-PC>
2024-10-09 17:23:29 +0530

    test case change

--- src/proj1/TestCase12.java
+++ src/proj1/TestCase12.java
@@ -3,7 +3,7 @@ package proj1;
 public class TestCase12 {
 
 	public static void main(String[] args) {
-		System.out.println("test 22 is done..."); 
+		System.out.println("test 222 is done..."); 
 	}
 
 }
